70b2d6420f1cfc4e9c81549d84dd8a7411b8514a
Valeriy Kharitonov <Onium93@gmail.com>
2022-08-19 18:45:20 +0300

    Initial Commit

---
+++ Lesson 1 Valeriy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1 Valeriy
+//
+//  Created by Valeriy Kharitonov on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
